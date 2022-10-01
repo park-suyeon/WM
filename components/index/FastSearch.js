fb3851f9ca69c6f0e762db3baffeb4a200c35949
park-suyeon <ssyyee98@sookmyung.ac.kr>
2022-10-01 21:58:03 +0900

    10.01_suyeon

--- components/index/FastSearch.js
+++ components/index/FastSearch.js
@@ -1,39 +1,48 @@
 import styled from "styled-components";
 const FastSearchBlock = styled.div`
   height: 50px;
-  position: fixed;
-  background-color: white;
-  top: 40px;
-  left: 140px;
-  width: 600px;
-  bottom: 40px;
-  border-radius: 20px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  overflow: scroll;
   align-items: center;
-  padding: 30px;
+  & > .searchWrap {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 20px;
+    display: flex;
 
-  .icon {
-    margin-bottom: 25px;
-    cursor: pointer;
+    padding: 6px;
+    margin-right: 6px;
+    align-items: center;
+    background-color: white;
+    & > .icon {
+      cursor: pointer;
+      margin: 5px;
+      height: 12px;
+    }
+    & > .text {
+      cursor: pointer;
+      font-size: 11px;
+      white-space: nowrap;
+    }
   }
 `;
 const FastSearch = (props) => {
   return (
     <FastSearchBlock>
-      <div>
-        <img className="icon" src="\images\icon\lift.png" />
-        휠체어 리프트
-        <img className="icon" src="\images\icon\charge.png" />
-        휠체어 급속충전기
-        <img className="icon" src="\images\icon\elevator.png" />
-        승강기
+      <div className="searchWrap">
         <img className="icon" src="\images\icon\ramp.png" />
-        경사로
-        <img className="icon" src="\images\icon\division.png" />
-        <img className="icon" src="\images\icon\more.png" />
+        <div className="text">경사로</div>
+      </div>
+      <div className="searchWrap">
+        <img className="icon" src="\images\icon\charge.png" />
+        <div className="text">휠체어 급속충전기</div>
+      </div>
+      <div className="searchWrap">
+        <img className="icon" src="\images\icon\lift.png" />
+        <div className="text">휠체어 리프트</div>
+      </div>
+      <div className="searchWrap">
+        <img className="icon" src="\images\icon\elevator.png" />
+        <div className="text">승강기</div>
       </div>
     </FastSearchBlock>
   );
