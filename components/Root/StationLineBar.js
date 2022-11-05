74e0cabba227effec6f8db0770d4d217342e9038
yeon1211 <dwd1022@naver.com>
2022-11-05 12:12:23 +0900

    221105after

--- components/Root/StationLineBar.js
+++ components/Root/StationLineBar.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import TransferLine from "./TransferLine";
+import TransferBlock from "./TransferLine";
 const StationWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -10,7 +12,7 @@ const StationWrapper = styled.div`
     justify-content: center;
     width: 100%;
     height: 20px;
-    padding-bottom: 10px;
+    padding: 30px 0px 30px 0px;
     align-items: center;
     .LineBar {
       display: flex;
@@ -19,99 +21,58 @@ const StationWrapper = styled.div`
       border-radius: 99px;
       border-bottom: 20px solid #d9d9d9;
     }
-    .TransferWrapper {
-      display: flex;
-      align-items: center;
-      position: absolute;
-      padding-bottom: 3px;
-      .TransferCircle1 {
-        display: flex;
-        align-items: center;
-        position: relative;
-        z-index: 2;
-        left: 150%;
-        justify-content: center;
-        border-radius: 99999px;
-        width: 30px;
-        height: 30px;
-        background-color: #3cb449;
-        .Transfertext {
-          font-size: 18px;
-          font-weight: bolder;
-          color: white;
-        }
-      }
-      .TransferLineBar1 {
-        display: flex;
-        position: absolute;
-        left: 170%;
-        width: 110px;
-        height: 20px;
-        z-index: 1;
-        align-items: center;
-        border-radius: 99px;
-        background-color: #3cb449;
-        .TransferTime1 {
-          position: absolute;
-          display: flex;
-          align-items: center;
-          left: 40%;
-          font-size: 12px;
-          color: white;
-        }
-      }
-      .TransferLineBar2 {
-        display: flex;
-        position: absolute;
-        left: 450%;
-        width: 150px;
-        height: 20px;
-        z-index: 1;
-        align-items: center;
-        border-radius: 99px;
-        background-color: #0ab5eb;
-        .TransferTime2 {
-          position: absolute;
-          display: flex;
-          align-items: center;
-          left: 50%;
-          font-size: 12px;
-          color: white;
-        }
-      }
 
-      .TransferCircle2 {
+    .TransferLineBar2 {
+      display: flex;
+      position: absolute;
+      left: 450%;
+      width: 150px;
+      height: 20px;
+      z-index: 1;
+      align-items: center;
+      border-radius: 99px;
+      background-color: #0ab5eb;
+      .TransferTime2 {
+        position: absolute;
         display: flex;
         align-items: center;
-        position: relative;
-        z-index: 2;
-        left: 400%;
-        justify-content: center;
-        border-radius: 99999px;
-        width: 30px;
-        height: 30px;
-        background-color: #0ab5eb;
-        .Transfertext {
-          font-size: 18px;
-          font-weight: bolder;
-          color: white;
-        }
+        left: 50%;
+        font-size: 12px;
+        color: white;
       }
     }
-    .iconCircle {
+
+    .TransferCircle2 {
       display: flex;
       align-items: center;
-      position: absolute;
+      position: relative;
+      z-index: 2;
+      left: 400%;
       justify-content: center;
       border-radius: 99999px;
-      width: 25px;
-      height: 25px;
-      background-color: #c4c4c4;
-      .icon {
-        width: 15px;
-        height: 16px;
-        margin: 2px;
+      width: 30px;
+      height: 30px;
+      background-color: #0ab5eb;
+      .Transfertext {
+        font-size: 18px;
+        font-weight: bolder;
+        color: white;
       }
+    }
+  }
+  .iconCircle {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    justify-content: center;
+    border-radius: 99999px;
+    width: 25px;
+    height: 25px;
+    background-color: #c4c4c4;
+    .icon {
+      width: 15px;
+      height: 16px;
+      margin: 2px;
     }
   }
 
@@ -151,21 +112,8 @@ const StationLine = (props) => {
         <div className="LineBar">
           <div className="iconCircle">
             <img className="icon" src="/images/icon/wheelchair.png" />
-
-            <div className="TransferWrapper">
-              <div className="TransferCircle1">
-                <div className="Transfertext">2</div>
-              </div>
-              <div className="TransferLineBar1">
-                <div className="TransferTime1">6분</div>
-              </div>
-              <div className="TransferCircle2">
-                <div className="Transfertext">4</div>
-              </div>
-              <div className="TransferLineBar2">
-                <div className="TransferTime2">6분</div>
-              </div>
-            </div>
+            <TransferLine lineText={2} time={6} left={100}></TransferLine>
+            <TransferLine lineText={4} time={10} left={800}></TransferLine>
           </div>
         </div>
       </div>
---
+++ config/subwayLineColor.js
@@ -0,0 +1,4 @@
+export const subwayLineColor = {
+  2: "#3CB449",
+  4: "#0AB5EB",
+};
