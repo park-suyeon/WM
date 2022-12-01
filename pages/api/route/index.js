d2f552cbc4d78a1267132b5175412d17398c5bff
park-suyeon <ssyyee98@sookmyung.ac.kr>
2022-12-01 22:54:42 +0900

    ee

--- pages/api/route/index.js
+++ pages/api/route/index.js
@@ -1,28 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const axios = require('axios');
-const data = require('../../../components/testPublicTrans.json');
+const axios = require("axios");
+const data = require("../../../components/testPublicTrans.json");
 export default async function handler(req, res) {
   const { startX, startY, endX, endY } = req.body;
 
-  console.log(startX, startY, endX, endY);
+  // console.log(startX, startY, endX, endY);
   // if (!startX || !startY || !endX || !endY)
-  //   return res.status(400).send('없는 값');
+  //   return res.status(400).send("없는 값");
 
   // const { data } = await axios({
-  //   method: 'post',
-  //   url: 'https://apis.openapi.sk.com/transit/routes',
+  //   method: "post",
+  //   url: "https://apis.openapi.sk.com/transit/routes",
   //   data: {
   //     startX,
   //     startY,
   //     endX,
   //     endY,
   //     lang: 0,
-  //     format: 'json',
+  //     format: "json",
   //     count: 10,
   //   },
   //   headers: {
-  //     accept: 'application/json',
-  //     'content-type': 'application/json',
+  //     accept: "application/json",
+  //     "content-type": "application/json",
   //     appKey: process.env.NEXT_PUBLIC_TMAP_CLIENT_ID,
   //   },
   // });
@@ -31,13 +31,13 @@ export default async function handler(req, res) {
   const tempI = JSON.parse(JSON.stringify(itineraries));
   const lessTransfer = tempI.sort((a, b) => {
     return (
-      a.legs.filter((ia) => ia.mode === 'TRANSFER').length -
-      b.legs.filter((ia) => ia.mode === 'TRANSFER').length
+      a.legs.filter((ia) => ia.mode === "TRANSFER").length -
+      b.legs.filter((ia) => ia.mode === "TRANSFER").length
     );
   })[0];
   const onlySubway =
     itineraries.find((item) => {
-      return !item.legs.some((leg) => leg.mode === 'BUS');
+      return !item.legs.some((leg) => leg.mode === "BUS");
     }) || lessTransfer;
   const faster = itineraries.sort((a, b) => {
     return a.totalTime - b.totalTime;
